e8be97fad44f493751922b1a821726a58d48a712
xcode73 <xcode.uln@gmail.com>
2022-02-07 18:10:59 +0400

    Initial Commit

---
+++ DailyPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyPlanner
+//
+//  Created by Nikolai Eremenko on 07.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
